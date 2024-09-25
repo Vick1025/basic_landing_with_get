ae1735ddf1d37ebca203fa4c7b6dbc434d6637aa
AN CHE WEI <vick.an@thortron.com>
2024-09-25 15:43:25 +0800

    Align naming rules

--- lib/core/widgets/navigate/bottom_navi_bar.dart
+++ lib/core/widgets/navigate/bottom_navi_bar.dart
@@ -1,6 +1,6 @@
 import 'package:basic_landing_with_get/core/controllers/setting_controller.dart';
 import 'package:basic_landing_with_get/features/book_search/presentation/controllers/book_search_controller.dart';
-import 'package:basic_landing_with_get/features/landing_navigate/business/entities/navi_item_entity.dart';
+import 'package:basic_landing_with_get/features/landing_navigate/domain/entities/navi_item_entity.dart';
 import 'package:basic_landing_with_get/features/landing_navigate/presentation/controllers/landing_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -41,7 +41,7 @@ class BottomNaviBar extends GetView<SettingController> {
           return InkWell(
             borderRadius: BorderRadius.circular(20.0),
             onTap: () {
-              if(controller.currentBottomNaviSelect.value == index){
+              if (controller.currentBottomNaviSelect.value == index) {
                 return;
               }
               controller.currentBottomNaviSelect.value = index;
